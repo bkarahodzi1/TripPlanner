297a9336fd0de551a29000453567abf5fe8c3ffe
bkarahodzi1 <bkarahodzi1@etf.unsa.ba>
2023-12-08 19:11:15 +0100

    This commit will add the page new_trip/tourist_gudie, and make the choice between which elements should be loaded depending on what button the user clicked.

---
+++ frontend/js/new_trip.js
@@ -0,0 +1,12 @@
+function onLoad(){
+    let option = localStorage.getItem('selectedOption');
+    if (option == 'new')
+    {
+
+    }
+    else if(option == 'guide')
+    {
+        let polazna = document.getElementById("startPoint_p");
+
+    }
+}
