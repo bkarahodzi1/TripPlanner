d2b17fe0b5e189e7734fd5f2397de337b261d0a7
kenankd <kdizdarevi1@etf.unsa.ba>
2023-12-08 23:01:39 +0100

    implement logic for planning trip day by day

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,6 +1,7 @@
 import  express  from "express";
 import  fs  from 'fs';
-import { getLocations } from "../text-completion/requests.js";
+
+import { getLocations, planTrip } from "../text-completion/requests.js";
 import bodyParser from 'body-parser';
 import { getEventListeners } from "events";
 import { generateText } from "../image recognition/textGeneration.js";
@@ -16,19 +17,15 @@ router.post('/locations', async (req,res)=>{
     if (base64 != undefined){
         image_description = generateText(base64);
     } 
-    
-  
     let body = req.body;
-    
-    
     let locations = await getLocations(body['start_point'], body['interests'], body['budget'], body['categories'], body['trip_length'],image_description);
-    
-    
     res.send(locations);
+});
 
-
-
-
+router.post('/plan',async (req,res) => {
+    const {start_point,budget,categories,trip_length,location}=req.body
+    let days = await planTrip(start_point,budget,categories,trip_length,location)
+    res.send(days)
 });
 
 
