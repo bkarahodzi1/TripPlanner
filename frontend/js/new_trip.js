a56df88989cfdb2663baf00d412e439865adae75
bkarahodzi1 <bkarahodzi1@etf.unsa.ba>
2023-12-08 19:37:16 +0100

    Fix new_trip.js. Refactor the names. of the variables.

--- frontend/js/new_trip.js
+++ frontend/js/new_trip.js
@@ -1,14 +1,18 @@
-function onLoad(){
+function onLoadPage(){
     let option = localStorage.getItem('selectedOption');
     if (option == 'new')
     {
+        console.log("if")
 
     }
     else if(option == 'guide')
     {
         let polazna = document.getElementById("startPoint_p");
         polazna.remove();
+        console.log("else if")
         let naslov = document.title;
         naslov = "Tourist guide";
     }
 }
+
+window.onLoad = onLoadPage();
