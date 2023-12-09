3ff21e010777aa92fef1d6d7aa8f9860576f51c9
nedimkrupalija <krupalijanedim@gmail.com>
2023-12-09 02:01:40 +0100

    route for planning trip from current location

--- backend/text-completion/requests.js
+++ backend/text-completion/requests.js
@@ -10,6 +10,30 @@ const openai = new OpenAI({
     apiKey: process.env.apiKey
 });
   
+export async function planTripFromCurrentLocation(location, trip_length, budget, categories=undefined){
+    let content = `Provide me with a daily plan of my stay in location ${location}.
+    My budget is ${budget} euros, I am staying for ${trip_length} days.`;
+    if (categories!=undefined){
+        content += `I would love to see and do something related to  ${categories.join(', ')}`
+    }
+    content+= "Include costs only for food, sightseeing, accomodation. I want you to give me current day numerated from 1., title of what I will be doing that day,"
+    + " description of that activity, time it is going to take, approximate price. Result is mandatory to be in JSON in this form: array of days with keys day as int type, title, description, time,"
+    + " approximate_price as int type. If approximate price is free put it as 0. If all approximate prices added can't fit inside my budget or for some other reasons return JSON object with key error and value of your explanation. DONT PUT ANYTHING THAT ISNT JSON FORMAT AS SPECIFIED"
+   
+    const stream = await openai.chat.completions.create({
+        model: 'gpt-4',
+        messages: [{ role: 'user', content: `${content}` }],
+      });
+
+      try {
+        return JSON.parse(stream.choices[0].message.content.toString());
+      } catch (error) {
+        return {"error" : "Something went wrong! Try to change your parameters."};
+      }
+
+}
+
+
 export async function getLocations(start_point, interests, budget, categories, trip_length, image_description=null) {
     let content=`You should give me up to 3 suggested locations for my trip.
     My start point is from ${start_point}. My interests include ${interests}.
